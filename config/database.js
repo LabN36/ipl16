b00692eb0565127b94c76edcfc0b0b8d342252c3
Rishabh Agrawal <ragrawal804@gmail.com>
2016-04-10 18:19:36 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://ragrawal804:Ragrawal@804@ds021010.mlab.com:21010/ipl16',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
